b7baf2a6ca72aa8a3ee2d675a024f2f4a6995d78
saravanan1225 <60806369+saravanan1225@users.noreply.github.com>
2020-11-20 12:49:54 +0530

    Update database.inc.php

--- database.inc.php
+++ database.inc.php
@@ -1,4 +1,4 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
-$con=mysqli_connect('localhost','root','','online_food');
+$con=mysqli_connect('sql12.freemysqlhosting.net','sql12377713','fP7AHslfd5','sql12377713');
 ?>
